e20cb147a911884ec9130431f0f4a1fbcecc4de4
LozzDev <lozzdeveloper@gmail.com>
2025-02-06 11:27:22 +0100

    Add Card component with styling and Bootstrap integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { RouterProvider } from 'react-router-dom';
 import { Router } from './router/Router.jsx';
 createRoot(document.getElementById('root')).render(

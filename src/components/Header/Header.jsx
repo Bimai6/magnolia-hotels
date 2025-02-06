a982e73a2b5e07643e5aa82bf970c2aaef85f4b4
PhilippeInCode <felipechacon.montero@alumnos.cei.es>
2025-02-06 10:57:43 +0100

    Add header to all pages and connect all the routes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 
 const navItems = [
-  { label: 'Inicio', value: 'home', icon: '/icons/isotype.svg', width: 40, height: 40},
-  { label: 'Estancia', value: 'book', icon: '/icons/bed_2.svg' , width: 34, height: 34},
-  { label: 'Restaurante', value: 'restaurant', icon: '/icons/restaurant.svg', width: 28, height: 28},
-  { label: 'Contacto', value: 'contact', icon: '/icons/contact.svg', width: 28, height: 28 },
-  { label: 'Perfil', value: 'profile', icon: '/icons/profile.svg', width: 28, height: 28 },
+  { label: 'Inicio', value: 'home', icon: '/icons/isotype.svg', width: 40, height: 40, link: '/'},
+  { label: 'Estancia', value: 'book', icon: '/icons/bed_2.svg' , width: 34, height: 34, link: '/my-reservations'},
+  { label: 'Restaurante', value: 'restaurant', icon: '/icons/restaurant.svg', width: 28, height: 28, link: '/restaurant'},
+  { label: 'Contacto', value: 'contact', icon: '/icons/contact.svg', width: 28, height: 28},
+  { label: 'Perfil', value: 'profile', icon: '/icons/profile.svg', width: 28, height: 28, link: '/profile'},
 ];
 
-function HeaderMobile() {
+function Header() {
   const [value, setValue] = React.useState('home');
 
   const handleChange = (event, newValue) => {
@@ -22,6 +23,8 @@ function HeaderMobile() {
       {navItems.map((item) => (
         <BottomNavigationAction
           key={item.value}
+          component={Link}
+          to={item.link}
           value={item.value}
           icon={<img src={item.icon} alt={item.label} width={item.width} height={item.height} />}
         />
@@ -30,4 +33,4 @@ function HeaderMobile() {
   );
 }
 
-export default HeaderMobile;
+export default Header;

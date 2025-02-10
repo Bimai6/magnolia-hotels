8d9163fa8836ffb8bdd6e12c754fd062c0ba8aca
Bimai6 <bimaivfx.gmail.com>
2025-02-10 11:37:52 +0100

    Add buttonSearch//TODO rename it, add hover

--- src/components/buttonSearch/ButtonSearch.jsx
+++ src/components/buttonSearch/ButtonSearch.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './ButtonSearch.css';
+import {Button} from '@mui/material';
 
 const ButtonSearch = () => {
   return (
-    <div>ButtonSearch</div>
+    <Button className='d-flex position-fixed shadow-none text-white btn-custom' variant="contained" startIcon={
+      <img
+        src="https://res.cloudinary.com/demqnwfff/image/upload/v1739175805/bed_1_ahw0kw.svg"
+        alt="bed icon"
+      />
+    }>
+    RESERVAR
+    </Button>
   )
 }
 
-export default ButtonSearch
+export default ButtonSearch;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header/Header';
+import Button from '../components/buttonSearch/ButtonSearch';
 
 const Home = () => {
   return (
-    <>Home
+    <>
       <Header />
+      <Button />
     </>
   )
 }

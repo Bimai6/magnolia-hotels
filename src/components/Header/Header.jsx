642a6a7153c220ae9e0cbece9ef6672bda1832a5
PhilippeInCode <felipechacon.montero@alumnos.cei.es>
2025-02-12 11:08:35 +0100

    Fix Desktop and Mobile Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 import { AppBar, Toolbar, IconButton, Box, BottomNavigation, BottomNavigationAction, useMediaQuery, Button} from '@mui/material';
 
 const navItems = [
-  { label: 'Inicio', value: 'home', icon: 'https://res.cloudinary.com/dk1g12n2h/image/upload/v1738865406/isotype_idmria.svg', link: '/', width: 40, height: 40 },
-  { label: 'Estancia', value: 'book', icon: 'https://res.cloudinary.com/dk1g12n2h/image/upload/v1738865406/bed_2_kda2wz.svg', link: '/my-reservations', width: 34, height: 34, ml: 'auto'},
-  { label: 'Restaurante', value: 'restaurant', icon: 'https://res.cloudinary.com/dk1g12n2h/image/upload/v1738865406/restaurant_d16z1c.svg', link: '/restaurant', width: 28, height: 28, ml:2 },
-  { label: 'Contacto', value: 'contact', icon: 'https://res.cloudinary.com/dk1g12n2h/image/upload/v1738865406/contact_incmt5.svg', width: 28, height: 28, ml:2 },
-  { label: 'Perfil', value: 'profile', icon: 'https://res.cloudinary.com/dk1g12n2h/image/upload/v1738865406/profile_d9xatr.svg', link: '/profile', width: 28, height: 28, ml:2 },
+  { label: 'Inicio', value: 'home', icon: 'https://res.cloudinary.com/dk1g12n2h/image/upload/v1738865406/isotype_idmria.svg', link: '/', width: 45, height: 45 },
+  { label: 'Estancia', value: 'book', icon: 'https://res.cloudinary.com/dk1g12n2h/image/upload/v1738865406/bed_2_kda2wz.svg', link: '/my-reservations', width: 39, height: 39, ml: 'auto'},
+  { label: 'Restaurante', value: 'restaurant', icon: 'https://res.cloudinary.com/dk1g12n2h/image/upload/v1738865406/restaurant_d16z1c.svg', link: '/restaurant', width: 33, height: 33, ml:2 },
+  { label: 'Contacto', value: 'contact', icon: 'https://res.cloudinary.com/dk1g12n2h/image/upload/v1738865406/contact_incmt5.svg', width: 33, height: 33, ml:2 },
+  { label: 'Identificarse', value: 'profile', icon: 'https://res.cloudinary.com/dk1g12n2h/image/upload/v1738865406/profile_d9xatr.svg', link: '/profile', width: 33, height: 33, ml:2 },
 ];
 
 const MobileHeader = () => {
@@ -49,45 +49,51 @@ function DesktopHeader() {
             size="large"
             edge="start"
             aria-label="logo"
+            disableRipple
           >
             <img 
               src="https://res.cloudinary.com/dk1g12n2h/image/upload/v1739173714/IMG-20250202-WA0012_1_eic08v.png" 
               alt="Logo"
-              width={navItems[0].width}
-              height={navItems[0].height}
-              style={{ objectFit: 'contain', width: '200px' }} 
+              style={{ objectFit: 'contain', width: '200px', height: 'auto' }} 
             />
           </IconButton>
-          {navItems.slice(1).map((item) => (
-            <Button 
-              key={item.value} 
-              component={Link} 
-              to={item.link} 
-              sx={{ color: 'black', textTransform: 'none', fontSize: '16px', ml: item.ml, position: 'relative',
-                '&::after': {
-                  content: '""',
-                  display: 'block',
-                  width: 0,
-                  height: '2px',
-                  background: 'black',
-                  transition: 'width .3s',
-                  position: 'absolute',
-                  bottom: 0,
-                  left: 0
-                },
-                '&:hover::after': {
-                  width: '100%'
-                }
-              }}
-            >
-              {item.label}
-            </Button>
-          ))}
+          <Box sx={{ ml: 'auto', display: 'flex', gap: 8 }}>
+            {navItems.slice(1).map((item) => (
+              <Button 
+                key={item.value} 
+                component={Link} 
+                to={item.link} 
+                sx={{ 
+                  color: 'black', 
+                  textTransform: 'none', 
+                  fontSize: '18px', 
+                  position: 'relative',
+                  '&::after': {
+                    content: '""',
+                    display: 'block',
+                    width: 0,
+                    height: '2px',
+                    background: 'black',
+                    transition: 'width .3s',
+                    position: 'absolute',
+                    bottom: 0,
+                    left: 0
+                  },
+                  '&:hover::after': {
+                    width: '100%'
+                  }
+                }}
+              >
+                {item.label}
+              </Button>
+            ))}
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
   );
 }
+
 
 
 function Header() {

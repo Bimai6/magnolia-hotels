ddfd5f05456aec0736fe520315109dec41c44da4
alegomoj <alegomoj@gmail.com>
2025-02-13 09:58:38 +0100

    Created restaurant page

--- src/components/ButtonRestaurant/ButtonRestaurant.jsx
+++ src/components/ButtonRestaurant/ButtonRestaurant.jsx
@@ -4,9 +4,7 @@ import "./ButtonRestaurant.css";
 const ButtonRestaurant = ({title, action}) => {
     return (
         <body>
-            <div id="pageButtons">
              <button type="button" className="btn btn-primary btn-lg magnolia-restaurant" onClick={action}>{title}</button>
-            </div>
         </body>
     );
   };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import Footer from '../components/Footer/Footer'
-import Header from '../components/Header/Header'
+import React from 'react';
+import Footer from '../components/Footer/Footer';
+import Header from '../components/Header/Header';
 import ButtonRestaurant from '../components/ButtonRestaurant/ButtonRestaurant';
+import Restaurant from './Restaurant';
 
 const Home = () => {
   return (
   <div>
-  <ButtonRestaurant />
+  <Restaurant />
   </div>
   )
 }

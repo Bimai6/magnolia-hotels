9e5322a042a38bcbc88ed724f6792cd469d7add0
pablo-jm <pablojime1998@gmail.com>
2025-02-16 20:20:44 +0100

    Menu Structure

--- src/pages/RestaurantMenu/RestaurantMenu.jsx
+++ src/pages/RestaurantMenu/RestaurantMenu.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import { useState } from "react";
+import "./RestaurantMenu.css";
 
-const RestaurantMenu = () => {
+const RestaurantMenu = ({ closeMenu }) => {
+  const [page, setPage] = useState(1);
+
   return (
-    <div>RestaurantMenu</div>
-  )
-}
+    <div className="overlay">
+      <button className="closeMenu" onClick={closeMenu}>
+        ✖
+      </button>
+
+      <div className="menu">
+        <div className="content">
+          {pagina === 1 ? (
+            <>
+              <div className="column left-column">
+        
+              </div>
+              <div className="column right-column">
+                
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="column left-column">
+                
+              </div>
+              <div className="column right-column">
+                
+              </div>
+            </>
+          )}
+        </div>
+
+        <button
+          className="page-change"
+          onClick={() => setPage(page === 1 ? 2 : 1)}
+        >
+          →
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default RestaurantMenu;

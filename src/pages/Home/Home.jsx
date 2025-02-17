37fbabd3bbeb537174946e33d5e062ac33561271
PhilippeInCode <felipechacon.montero@alumnos.cei.es>
2025-02-17 10:30:08 +0100

    modify home page structure (uncomplete)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import ButtonSearch from '../../components/ButtonSearch/ButtonSearch';
 import { Box, Grid, Typography, Container } from '@mui/material';
 
 const Home = () => {
@@ -17,85 +19,92 @@ const Home = () => {
       <Header />
       <Container maxWidth="lg">
 
-      <Grid container spacing={4} alignItems="center" sx={{ my: 4 }}>
-        <Grid item xs={12} md={6}>
-          <Typography variant="h3" fontWeight="bold">
-            Tus vacaciones como nunca
-          </Typography>
-          <Typography variant="body1" sx={{ mt: 2 }}>
-          Lorem fistrum condemor me cago en tus muelas amatomaa te va a hasé pupitaa a gramenawer benemeoor. Hasta lu
-          ego Lucas jarl ese que llega mamaar. Sexuarl a gramenawer te voy a borrar el cerito papaar papaar me cago en tus muelas. Tiene musho peligro.
-          </Typography>
-        </Grid>
-        <Box sx={{
-            position: 'absolute',
-            top: 0,
-            right: 0,
-            width: { xs: '100%', md: '50%' },
-            height: '100%',
-            zIndex: -1
+       <Box sx={{ my: 4 }}>
+          <Grid container spacing={4} alignItems="center">
+            <Grid item xs={12} md={6} sx={{ ml: -8, mt: 20 }}>
+              <Typography variant="h3" fontWeight="bold">
+                Tus vacaciones como nunca
+              </Typography>
+              <Typography variant="body1" sx={{ mt: 2 }}>
+              Lorem fistrum condemor me cago en tus muelas amatomaa te va a hasé pupitaa a gramenawer benemeoor. Hasta lu
+              ego Lucas jarl ese que llega mamaar. Sexuarl a gramenawer te voy a borrar el cerito papaar papaar me cago en tus muelas. Tiene musho peligro.
+              </Typography>
+            </Grid>
+            <Box sx={{
+                position: 'absolute',
+                top: 0,
+                right: 0,
+                width: { xs: '100%', md: '50%' },
+                height: '100%',
+                zIndex: -1
               }}>
-          <img 
-            src="https://res.cloudinary.com/dk1g12n2h/image/upload/v1739175372/fondo_hp9223.png" 
-            alt="Hotel Magnolia"
-            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-          />
+              <img 
+                src="https://res.cloudinary.com/dk1g12n2h/image/upload/v1739175372/fondo_hp9223.png" 
+                alt="Hotel Magnolia"
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+              />
+            </Box>
+          </Grid>
         </Box>
-      </Grid>
 
-<Grid 
-  container 
-  spacing={4} 
-  justifyContent="center" 
-  sx={{ 
-    my: 4, 
-    minHeight: '150vh',
-    '& .MuiGrid-item': { 
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center'
-    } 
-  }} 
->
-  {benefits.map((benefit, index) => (
-    <Grid item xs={12} sm={6} md={3} key={index}>
-      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <img src={benefit.icon} alt={benefit.label} width="150" />
+        <Box sx={{ mt: 40, minHeight: '150vh' }}>
+          <Grid 
+            container 
+            spacing={4} 
+            justifyContent="center"
+            sx={{ 
+              '& .MuiGrid-item': { 
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center'
+              } 
+            }} 
+          >
+            {benefits.map((benefit, index) => (
+              <Grid item xs={12} sm={6} md={3} key={index}>
+                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                  <img src={benefit.icon} alt={benefit.label} width="150" />
+                  <Typography 
+                    variant="subtitle1" 
+                    sx={{ 
+                      position: 'relative', 
+                      left: '10px',
+                      mt: 2, 
+                      textAlign: 'center',
+                      minHeight: '4em', 
+                      display: 'flex',
+                      align: 'center'
+                    }}>
+                    {benefit.label}
+                  </Typography>
+                </Box>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
 
-        <Typography 
-          variant="subtitle1" 
-          sx={{ 
-          position: 'relative', left: '10px',
-          mt: 2, 
-          textAlign: 'center',
-          minHeight: '4em', 
-          display: 'flex',
-          align: 'center'
-              }}>
-          {benefit.label}
-        </Typography>
-        
-      </Box>
-    </Grid>
-  ))}
-</Grid>
+        <Box sx={{ mt: -100}}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3105.123456789!2d-5.984459!3d37.389092!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd126f6e5a2a3b4f%3A0xabcdef1234567890!2sCalle%20Pedro%20del%20Toro%2018%2C%20Sevilla%2C%20España!5e0!3m2!1ses!2ses!4v1610000000000!5m2!1ses!2ses" 
+                width="100%" 
+                height="300" 
+                style={{ border: 0, borderRadius: '10px'}}
+                allowFullScreen 
+                loading= "lazy"
+              />
+            </Grid>
+          </Grid>
+        </Box>
 
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <iframe 
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3105.123456789!2d-5.984459!3d37.389092!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd126f6e5a2a3b4f%3A0xabcdef1234567890!2sCalle%20Pedro%20del%20Toro%2018%2C%20Sevilla%2C%20España!5e0!3m2!1ses!2ses!4v1610000000000!5m2!1ses!2ses" 
-            width="100%" 
-            height="300" 
-            style={{ border: 0, borderRadius: '10px'}}
-            allowFullScreen 
-            loading= "lazy"
-          />
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+      <ButtonSearch />
+      <Footer />
     </>
   )
 }
+
 
 export default Home;

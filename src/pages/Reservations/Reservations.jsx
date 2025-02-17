da4f516166adf7c2916d4eb74e30eff3401d7316
LozzDev <lozzdeveloper@gmail.com>
2025-02-17 09:26:57 +0100

    fix: add missing image properties to room data and clean up CSS files

--- src/pages/Reservations/Reservations.jsx
+++ src/pages/Reservations/Reservations.jsx
@@ -2,16 +2,11 @@ import React from 'react';
 import RoomCard from '../../components/roomCard/RoomCard';
 
 const rooms = [
-  { title: 'Habitación Doble', stars: 3, price: 43,   },
-  { title: 'Habitación Individual', stars: 4, price: 38,   },
-  { title: 'Suite Deluxe', stars: 5, price: 99,   },
-  { title: 'Habitación Familiar', stars: 4, price: 55,  },
-  { title: 'Habitación Económica', stars: 3, price: 30,   },
-  { title: 'Habitación Económica', stars: 3, price: 30,   },
-  { title: 'Habitación Económica', stars: 3, price: 30,   },
-  { title: 'Habitación Económica', stars: 3, price: 30,   },
-  { title: 'Habitación Económica', stars: 3, price: 30,   },
-  { title: 'Habitación Económica', stars: 3, price: 30,   },
+  { title: 'Habitación Doble', stars: 3, price: 43, img: 'https://res.cloudinary.com/dc732dg4w/image/upload/v1739780013/habitacion-superior_hdbplp.jpg'},
+  { title: 'Habitación Individual', stars: 4, price: 38, img: 'https://res.cloudinary.com/dc732dg4w/image/upload/v1739780013/habitacion-superior_hdbplp.jpg'  },
+  { title: 'Suite Deluxe', stars: 5, price: 99, img: 'https://res.cloudinary.com/dc732dg4w/image/upload/v1739780013/habitacion-superior_hdbplp.jpg'  },
+  { title: 'Habitación Superior', stars: 4, price: 55, img: 'https://res.cloudinary.com/dc732dg4w/image/upload/v1739780013/habitacion-superior_hdbplp.jpg' },
+  { title: 'Habitación Económica', stars: 3, price: 30, img: 'https://res.cloudinary.com/dc732dg4w/image/upload/v1739780013/habitacion-superior_hdbplp.jpg'  },
 ];
 
 //la variable room es un array de prueba para comprobar como se veria si extrayesemos los datos de una api o un json local

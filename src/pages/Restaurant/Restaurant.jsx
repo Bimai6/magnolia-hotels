c4d43e15a80b9e7eaf8262a70afb91b9a9cb30ef
alegomoj <alegomoj@gmail.com>
2025-02-17 09:36:30 +0100

    Added padding between restaurant title and buttons

--- src/pages/Restaurant/Restaurant.jsx
+++ src/pages/Restaurant/Restaurant.jsx
@@ -6,7 +6,7 @@ const Restaurant = () => {
   return (
     <div className="restaurant-container">
       <img src="https://res.cloudinary.com/dczjloaiy/image/upload/v1739180961/construction_16080058_1_iwgnb0.png" alt="Ebano Restaurant Logo" />
-      <img src="https://res.cloudinary.com/dczjloaiy/image/upload/v1739180953/Titulo_Logo_ej81de.png" alt="Ebano Restaurant Title" />
+      <img className='restaurantTitle' src="https://res.cloudinary.com/dczjloaiy/image/upload/v1739180953/Titulo_Logo_ej81de.png" alt="Ebano Restaurant Title" />
       <div id="buttonsRestaurant">
         <ButtonRestaurant title="Reservar" action="" />
         <ButtonRestaurant title="Modificar Reserva" action="" />

e5575984bff31dabb52c9f8179de947dc49db668
LozzDev <lozzdeveloper@gmail.com>
2025-02-26 09:57:25 +0100

    feat: implement room data fetching and filtering in Reservations page; update SearchResult component to handle date selection

--- src/pages/Reservations/Reservations.jsx
+++ src/pages/Reservations/Reservations.jsx
@@ -1,28 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import RoomCard from '../../components/RoomCard/RoomCard';
 import SearchResult from '../../components/SearchResult/SearchResult';
 import './Reservations.css';
-const rooms = [
-  { title: 'Habitación Doble',description:"Disfruta de la combinación perfecta entre elegancia y confort en nuestra Suite Junior, un espacio diseñado para brindar una estancia exclusiva y relajante.", stars: 3, price: 43, img: 'https://res.cloudinary.com/dc732dg4w/image/upload/v1739780013/habitacion-superior_hdbplp.jpg'},
-  { title: 'Habitación Individual',description:"Disfruta de la combinación perfecta entre elegancia y confort en nuestra Suite Junior, un espacio diseñado para brindar una estancia exclusiva y relajante.", stars: 4, price: 38, img: 'https://res.cloudinary.com/dc732dg4w/image/upload/v1739780013/habitacion-superior_hdbplp.jpg'  },
-  { title: 'Suite Deluxe', stars: 5,description:"Disfruta de la combinación perfecta entre elegancia y confort en nuestra Suite Junior, un espacio diseñado para brindar una estancia exclusiva y relajante.", price: 99, img: 'https://res.cloudinary.com/dc732dg4w/image/upload/v1739780013/habitacion-superior_hdbplp.jpg'  },
-  { title: 'Habitación Superior',description:"Disfruta de la combinación perfecta entre elegancia y confort en nuestra Suite Junior, un espacio diseñado para brindar una estancia exclusiva y relajante.", stars: 4, price: 55, img: 'https://res.cloudinary.com/dc732dg4w/image/upload/v1739780013/habitacion-superior_hdbplp.jpg' },
-  { title: 'Habitación Económica',description:"Disfruta de la combinación perfecta entre elegancia y confort en nuestra.", stars: 3, price: 30, img: 'https://res.cloudinary.com/dc732dg4w/image/upload/v1739780013/habitacion-superior_hdbplp.jpg'  },
-];
-
-//la variable room es un array de prueba para comprobar como se veria si extrayesemos los datos de una api o un json local
 
 const Reservations = () => {
+  const [rooms, setRooms] = useState([]);
+  const [filteredRooms, setFilteredRooms] = useState([]);
+  const [entry, setEntry] = useState(null); // Fecha de entrada
+  const [departure, setDeparture] = useState(null); // Fecha de salida
+
+  useEffect(() => {
+    fetch('http://localhost:3000/rooms')
+      .then(response => response.json())
+      .then(data => {
+        const availableRooms = data.filter(room => room.reservations.length === 0);
+        setRooms(availableRooms);
+      })
+      .catch(error => console.error('Error fetching rooms:', error));
+  }, []);
+
   return (
     <div className="container">
       <div className='search-result-container'>
-        <SearchResult />
+        <SearchResult setFilteredRooms={setFilteredRooms} rooms={rooms} setEntry={setEntry} setDeparture={setDeparture} />
       </div>
 
       <div className="row justify-content-center">
-        {rooms.map((room, index) => (
-          <div key={index} className="col-12 col-sm-6 col-lg-3 d-flex justify-content-center g-2">
-            <RoomCard {...room} />
+        {filteredRooms.map((room) => (
+          <div key={room.id} className="col-12 col-sm-6 col-lg-3 d-flex justify-content-center g-2">
+            <RoomCard {...room} setRooms={setRooms} entry={entry} departure={departure} />
           </div>
         ))}
       </div>

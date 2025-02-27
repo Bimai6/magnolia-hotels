e7569885ffc6d679f7a091f3daa80497530331c9
LozzDev <lozzdeveloper@gmail.com>
2025-02-27 20:15:34 +0100

    refactor: restructure Home component layout for improved responsiveness and styling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -27,54 +27,68 @@ const Home = () => {
     autoplay: true, 
     autoplaySpeed: 2000, 
     arrows: true, 
+    align: 'center',
   };
 
   const DesktopView = () => (
     <>
       <Header />
-      <Container maxWidth="lg">
-        <Box sx={{ my: 14 }}>
-          <Grid container spacing={4} alignItems="center">
-            <Grid item xs={12} md={6} sx={{ ml: -8, mt: 20 }}>
-            <Typography variant="h3" fontWeight="bold" sx={{ mt: 8, ml: 4, mr: 5, textAlign: { xs: 'justify', md: 'left' } }}>
-                Tus vacaciones como nunca
-              </Typography>
-              <Typography variant="body1" sx={{ mt: 8, ml: 4, mr: 5, textAlign: { xs: 'justify', md: 'left' } }}>
-                Descubre un oasis de lujo y confort donde cada detalle está pensado para hacer de tu estancia una experiencia inolvidable. Relájate en nuestras exclusivas instalaciones, disfruta de una gastronomía excepcional y sumérgete en un servicio diseñado para superar tus expectativas. Tu descanso y bienestar son nuestra prioridad. ¿Listo para unas vacaciones inigualables?
-              </Typography>
-            </Grid>
+      <div>
+        
+        {/* Contenedor Principal */}
+        <Grid container  alignItems="center" justifyContent="center" sx={{ position: "relative",   width: "100%", }}>
+          {/* Texto a la izquierda */}
+          <Grid item xs={12} md={6} sx={{ p: { xs: 2, md: 5 } }}>
+            <Typography 
+              variant="h2" 
+              fontWeight="bold" 
+              sx={{ mt: { xs: 4, md: 8 }, textAlign: { xs: "center", md: "left" } }}
+            >
+              Tus vacaciones como nunca
+            </Typography>
+            <Typography 
+              variant="body1" 
+              sx={{ mt: 2, textAlign:"justify" }}
+            >
+              Descubre un oasis de lujo y confort donde cada detalle está pensado para hacer de tu estancia una experiencia inolvidable. Relájate en nuestras exclusivas instalaciones, disfruta de una gastronomía excepcional y sumérgete en un servicio diseñado para superar tus expectativas. Tu descanso y bienestar son nuestra prioridad. ¿Listo para unas vacaciones inigualables?
+            </Typography>
+          </Grid>
+  
+          {/* Imagen de fondo */}
+          <Grid item xs={12} md={6} sx={{ position: "relative", display: "flex", alignItems: "stretch" }}>
             <Box sx={{
-              position: 'absolute',
-              top: 0,
-              right: 0,
-              width: { xs: '100%', md: '50%' },
-              height: '100%',
-              zIndex: -1
+              width: "100%",  // Asegura que ocupe todo el espacio del Grid
+              height: "100%", // Asegura que siempre tenga altura completa
+              overflow: "hidden",
             }}>
               <img 
                 src="https://res.cloudinary.com/dk1g12n2h/image/upload/v1739175372/fondo_hp9223.png" 
                 alt="Hotel Magnolia"
-                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                style={{ 
+                  width: "100%",  
+                  height: "100%",
+                  objectFit: "cover",
+                }}
               />
             </Box>
-          </Grid>
-        </Box>
-
-        <Box sx={{ display: 'flex', justifyContent: 'flex-start'}}>
-          <Grid container spacing={4} justifyContent="center" sx={{ '& .MuiGrid-item': { display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-end' } }}>
+          </Grid> 
+        </Grid>
+  
+        {/* Sección de beneficios */}
+        <Box sx={{ display: "flex", justifyContent: "center", mt: 5 }}>
+          <Grid container spacing={4} alignItems={"center"} justifyContent="center">
             {benefits.map((benefit, index) => (
-              <Grid item xs={12} sm={6} md={3} key={index}>
-                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                  <img src={benefit.icon} alt={benefit.label} width="150" />
-                  <Typography variant="subtitle1" sx={{ position: 'relative', left: '10px', mt: 2, textAlign: 'center', minHeight: '4em', display: 'flex', align: 'flex-start' }}>
-                    {benefit.label}
-                  </Typography>
-                </Box>
+              <Grid item xs={12} sm={6} md={3} key={index} sx={{ justifyItems: "center" }}>
+                <img src={benefit.icon} alt={benefit.label} width="100" />
+                <Typography variant="subtitle1" sx={{ mt: 2, minHeight: "4em" }}>
+                  {benefit.label}
+                </Typography>
               </Grid>
             ))}
           </Grid>
         </Box>
-
+  
+        {/* Mapa */}
         <Box sx={{ mt: 5 }}>
           <Grid container spacing={2}>
             <Grid item xs={12}>
@@ -82,18 +96,20 @@ const Home = () => {
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3105.123456789!2d-5.984459!3d37.389092!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd126f6e5a2a3b4f%3A0xabcdef1234567890!2sCalle%20Pedro%20del%20Toro%2018%2C%20Sevilla%2C%20España!5e0!3m2!1ses!2ses!4v1610000000000!5m2!1ses!2ses" 
                 width="100%" 
                 height="300" 
-                style={{ border: 0, borderRadius: '10px'}}
+                style={{ border: 0, borderRadius: "10px" }}
                 allowFullScreen 
                 loading="lazy"
               />
             </Grid>
           </Grid>
         </Box>
-      </Container>
+      </div>
+      
       <ButtonSearch />
       <Footer />
     </>
   );
+  
 
 
   const MobileView = () => (
@@ -135,11 +151,12 @@ const Home = () => {
             <Box key={index} sx={{ 
               mt: 8,
               display: "flex", 
-              flexDirection: "column", 
+              flexDirection: "row", 
               alignItems: "center", 
               justifyContent: "center", 
               textAlign: "center",
-              height: "300px" 
+              height: "230px",
+              
             }}>
           <img 
             src={benefit.icon} 
@@ -163,7 +180,7 @@ const Home = () => {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3105.123456789!2d-5.984459!3d37.389092!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd126f6e5a2a3b4f%3A0xabcdef1234567890!2sCalle%20Pedro%20del%20Toro%2018%2C%20Sevilla%2C%20España!5e0!3m2!1ses!2ses!4v1610000000000!5m2!1ses!2ses" 
             width="100%" 
             height="300" 
-            style={{ border: 0, borderRadius: '10px' }} 
+            style={{ border: 0, borderRadius: '10px', marginTop: '40px' }} 
             allowFullScreen 
             loading="lazy" 
           />

a2872cf4a90defde18d7527ce97b89c84484b984
Bimai6 <bimaivfx@gmail.com>
2025-10-27 15:35:56 +0100

    refactor: change AuthContext component, to handle token savings on frontend

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -5,29 +5,37 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [isLogged, setIsLogged] = useState(false);
   const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
     const savedUser = localStorage.getItem('user');
-    if (savedUser) {
-      setIsLogged(true);
+    const savedToken = localStorage.getItem('token');
+
+    if (savedUser && savedToken) {
       setUser(JSON.parse(savedUser));
+      setToken(savedToken);
+      setIsLogged(true);
     }
   }, []);
 
-  const login = (user) => {
+  const login = (user, token) => {
     localStorage.setItem('user', JSON.stringify(user));
-    setIsLogged(true);
+    localStorage.setItem('token', token);
     setUser(user);
+    setToken(token);
+    setIsLogged(true);
   };
 
   const logout = () => {
     localStorage.removeItem('user');
-    setIsLogged(false);
+    localStorage.removeItem('token');
     setUser(null);
+    setToken(null);
+    setIsLogged(false);
   };
 
   return (
-    <AuthContext.Provider value={{ isLogged, user, login, logout }}>
+    <AuthContext.Provider value={{ isLogged, user, token, login, logout }}>
       {children}
     </AuthContext.Provider>
   );

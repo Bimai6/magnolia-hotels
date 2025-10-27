4725a81880ed3bb0b0bc818b10bc764848b107b0
Bimai6 <bimaivfx@gmail.com>
2025-10-27 15:34:42 +0100

    feat: add login and register endpoints, add verifyToken middleware to PATCH routes

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { createUser, deleteAnUser, getAllUsers, getAnUser, updateAnUserProfile, updateAnUserReservation } from "../controllers/UserController.js";
+import { createUser, deleteAnUser, getAllUsers, getAnUser, loginUser, registerUser, updateAnUserProfile, updateAnUserReservation } from "../controllers/UserController.js";
 import { createRoom, createRooms, deleteARoom, getAllRooms, getARoom, updateARoomReservation } from "../controllers/RoomController.js";
 import User from "../models/UserModel.js";
 import Room from "../models/RoomModel.js";
+import { verifyToken } from "../middlewares/auth.js";
 
 const router = express.Router();
 
@@ -17,18 +18,21 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.post('/login', loginUser);
+router.post('/register', registerUser);
+
 router.get('/users', getAllUsers);
 router.get('/users/:id', getAnUser);
-router.post('/users', createUser);
-router.patch('/users/:id', updateAnUserProfile);
-router.patch('/users/:id/reservations', updateAnUserReservation);
-router.delete('/users/:id', deleteAnUser);
+//router.post('/users', createUser);
+router.patch('/users/:id', verifyToken, updateAnUserProfile);
+router.patch('/users/:id/reservations', verifyToken, updateAnUserReservation);
+//router.delete('/users/:id', deleteAnUser);
 
 router.get('/rooms', getAllRooms);
 router.get('/rooms/:id', getARoom);
-router.post('/rooms', createRoom);
-router.patch('/rooms/:id', updateARoomReservation);
-router.post('/rooms/bulk', createRooms);
-router.delete('/rooms/:id', deleteARoom);
+//router.post('/rooms', createRoom);
+router.patch('/rooms/:id', verifyToken, updateARoomReservation);
+//router.post('/rooms/bulk', createRooms);
+//router.delete('/rooms/:id', deleteARoom);
 
 export default router
